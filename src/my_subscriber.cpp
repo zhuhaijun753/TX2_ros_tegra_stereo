23e6997dd8d53efb7080b2e89cd7a180c9c27871
ShreyasSkandan <shreyas.ibanez@gmail.com>
2017-03-31 14:21:38 -0400

    read image pair

--- src/my_subscriber.cpp
+++ src/my_subscriber.cpp
@@ -2,12 +2,49 @@
 #include <image_transport/image_transport.h>
 #include <opencv2/highgui/highgui.hpp>
 #include <cv_bridge/cv_bridge.h>
+#include <opencv2/core/core.hpp>
+#include <opencv2/calib3d/calib3d.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/opencv.hpp>
+#include <stdio.h>
+#include <iostream>
+#include <ctime>
+#include <sys/time.h>
+
+#include <sstream>
+#include <iomanip>
+#include <string>
+#include <memory>
+
+//#include <NVX/nvx.h>
+//#include <NVX/nvx_timer.hpp>
+
+//#include <NVX/Application.hpp>
+//#include <NVX/ConfigParser.hpp>
+//#include <OVX/FrameSourceOVX.hpp>
+//#include <OVX/RenderOVX.hpp>
+//#include <NVX/SyncTimer.hpp>
+//#include <OVX/UtilityOVX.hpp>
+//#include <NVX/nvx_opencv_interop.hpp>
+
+//#include "stereo_matching.hpp"
+//#include "color_disparity_graph.hpp"
 
 void imageCallback(const sensor_msgs::ImageConstPtr& msg)
 {
+  cv_bridge::CvImagePtr cv_ptr;
+  cv::Mat img1,img2;
+  cv::Rect myROI_1(0,0,960,800); // constant 
+  cv::Rect myROI_2(0,800,960,800); // constant 
   try
   {
-    cv::imshow("view", cv_bridge::toCvShare(msg, "bgr8")->image);
+    cv_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::BGR8);
+    img1 = cv_ptr->image(myROI_1);
+    img2 = cv_ptr->image(myROI_2);
+
+    cv::imshow("view1", img1);
+    cv::imshow("view2", img2);  
+
     cv::waitKey(30);
   }
   catch (cv_bridge::Exception& e)
